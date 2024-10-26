ee10166b27fb29c24c26387e60cb09fe8a778b8b
Lucas Kang <kangzundao@gmail.com>
2024-10-26 14:06:55 -0400

    send data to frontend in JSON format

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from extract_keywords import extract_k
 from extract_quotes import extract_q
+import json
 
 app = Flask(__name__)
 llm_message = ""
@@ -17,8 +18,10 @@ def get_data():
     keywords = extract_k(llm_message)
     fact_check = extract_q(keywords, llm_message)
 
-    print(fact_check)
-    return jsonify({"status": "Message received!"}), 201
+    if isinstance(fact_check, str):
+        fact_check = json.loads(fact_check)
+
+    return jsonify(fact_check), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
